adb4ae254e5da8cb40009b0f8268e0e7714d114d
vivjoe <vivianiwegbu@gmail.com>
2023-03-16 18:00:12 -0700

    one

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - and that piece of art is useful - Dora Korpar
+* Return: This returns 1
+*/
+int main(void)
+{	write(2,
+	"and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n", 59);
+	return (1);
+}
