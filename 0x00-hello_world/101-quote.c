0bb0eddd0d92bbc133e1991149fcc9f39106ee3d
vivjoe <vivianiwegbu@gmail.com>
2023-03-16 19:28:45 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,6 @@
 * Return: This returns 1
 */
 int main(void)
-{	fwrite(2,
-	"and that piece of art is useful" .Dora Korpar, 2015 - 10 - 19\n", 59);
+{	write(1,"and that piece of art is useful" .Dora Korpar, 2015 - 10 - 19\n", 59);
 	return (1);
 }
