b3fb18db232cc003947cd8af667caadae3a19832
vivjoe <vivianiwegbu@gmail.com>
2023-03-16 21:54:29 -0700

    uu

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,6 +4,6 @@
 * Return: This returns 1
 */
 int main(void)
-{	write(1,"and that piece of art is useful" .Dora Korpar, 2015 - 10 - 19\n", 59);
+{	write(1,"and that piece of art is useful\"- Dora Korpar, 2015 - 10 - 19\n", 59);
 	return (1);
 }
