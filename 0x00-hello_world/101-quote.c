f3350335fd117f7faafc246747bf4218da8e7b57
vivjoe <vivianiwegbu@gmail.com>
2023-03-18 09:57:30 -0700

    operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,6 @@
 *Return: This returns 1
 */
 int main(void)
-{	fputs("and that piece of art is useful\" - Dora Korpar,
-2015-10-19\n", stdout);
+{	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19, stdout");
 	return (1);
 }
