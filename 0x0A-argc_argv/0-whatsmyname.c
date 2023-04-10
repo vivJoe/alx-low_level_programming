170d027738efb55db1748658d821cb9a5c1c85c8
vivjoe <vivianiwegbu@gmail.com>
2023-04-10 11:24:04 -0700

    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name, followed by a new line
+ * @argc: argument to count
+ * @argv[]: an array of argument
+ *
+ * Return: return 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
