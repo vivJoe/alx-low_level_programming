b2ca10dc5ed04310f1e9a0f1a063148535df529c
vivJoe <vivianiwegbu@gmail.com>
2023-04-27 15:10:03 -0700

    0x12. C - Singly linked lists

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+section .data
+	msg db 'Hello, Holberton', 0x0a, 0
+section .text
+	global main
+
+	extern printf
+
+main:
+	push rbp
+	mov rbp, rsp
+
+	mov edi, msg
+	xor eax, eax
+	call printf
+
+	mov rsp, rbp
+	pop rbp
+	xor eax, eax
+	ret
+
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the head of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *current, *next;
+
+	if (head == NULL)
+		return;
+
+	current = head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
+
