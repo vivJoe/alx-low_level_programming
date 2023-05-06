747dc96c984f7c7efd95e0cd8f21b4b59189a578
vivJoe <vivianiwegbu@gmail.com>
2023-05-06 10:55:48 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,8 @@
 * Return: This returns 0
 */
 int main(void)
-{	puts("\"Programming is like building a multilingual puzzle");
+{	
+	puts("Programming is like building a multilingual puzzle \n");
+
 	return (0);
 }
