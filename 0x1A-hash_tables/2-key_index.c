5ee81d614401ad5eaec0c559929bf95861816ec4
vivJoe <vivianiwegbu@gmail.com>
2023-07-07 15:26:30 -0700

    hash tables

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,51 +1,16 @@
 #include "hash_tables.h"
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: The hash table
- * @key: The key of the new element
- * @value: The value of the new element
+ * key_index - function that generates a distributed index
+ * according to a given hash
  *
- * Return: 1 on success, 0 on failure
+ * @key: key passed
+ * @size: size of the hash tables
+ * Return: key index
  */
-int
-hash_table_set(hash_table_t *ht, const char *key, const char *value)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int index = 0;
-	hash_node_t *new_hash_node = NULL;
-	hash_node_t *tmp = NULL;
-
-	if (!ht || !key || !(*key) || !value)
+	if (key == NULL || size == 0)
 		return (0);
-
-	index = key_index((unsigned char *)key, ht->size);
-	tmp = ht->array[index];
-
-	/* check if key exists */
-	while (tmp && strcmp(tmp->key, key) != 0)
-		tmp = tmp->next;
-
-	/* update value if key already exists */
-	if (tmp)
-	{
-		free(tmp->value);
-		tmp->value = strdup(value);
-		return (1);
-	}
-
-	/* add new node if key not found */
-
-	new_hash_node = malloc(sizeof(*new_hash_node));
-	if (!new_hash_node)
-		return (0);
-
-	new_hash_node->key = strdup(key);
-	new_hash_node->value = strdup(value);
-
-	new_hash_node->next = ht->array[index];
-	ht->array[index] = new_hash_node;
-
-	return (1);
+	return (hash_djb2(key) % size);
 }
